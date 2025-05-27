d334a4e9a7133a1a56c2446e02bd6bf03f41a3db
Yukitooo <yukibrent9@gmail.com>
2025-05-28 00:54:20 +0800

    Updated layout and styles

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,6 @@ function Intro({hideIntro}) {
   return (
     <> 
         <div className='intro'>
-            <img className='pikachu-dancing' src={pikachu}></img>
             <h1 className='heading'>WANT TO KNOW YOUR INNER POKEMON?</h1>
             <p>✨ Takes less than 5 minutes • Cute results guaranteed! ✨</p>
             <button className='btn' onClick={hideIntro}>Reveal My Pokemon!</button>
--- src/components/Result.js
+++ src/components/Result.js
@@ -2,29 +2,26 @@ import React from 'react'
 import personalityData from './personalityDescriptions'
 import './Result.css'
 
+function Result({ resultType }) {
+  const result = personalityData[resultType];
 
-function Result({resultType}) {
+  if (!result) return <p>Loading result...</p>;
 
-    const result = personalityData[resultType];
-
-    return (
-        <>
-            <div className='resultContent' style={{border: `1rem solid ${result.color}`}}>
-                <div className='leftSide' style={{borderRight: `2px ${result.color} solid`}}>
-                    <img src={result.image}></img>
-                    <h1 style={{color: result.color}} className='leftSideContent'>{result.name}</h1>
-                    <h3 style={{color: result.color}} className='leftSideContent'> {result.type}</h3>
-                    <p className='leftSideContent tagline'>{result.tagline}</p>
-                </div>
-                <div className='rightSide'>
-                    {result.description.map((desc) => 
-                        <p>- {desc}</p>
-                    )}
-                    
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className="resultContent" style={{ border: `1rem solid ${result.color}` }}>
+      <div className="leftSide" style={{ borderRight: `2px ${result.color} solid` }}>
+        <img src={`${process.env.PUBLIC_URL}/${result.image}`} alt={result.name} />
+        <h1 style={{ color: result.color }} className="leftSideContent">{result.name}</h1>
+        <h3 style={{ color: result.color }} className="leftSideContent">{result.type}</h3>
+        <p className="leftSideContent tagline">{result.tagline}</p>
+      </div>
+      <div className="rightSide">
+        {result.description.map((desc, index) => (
+          <p key={index}>- {desc}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Result
+export default Result;
