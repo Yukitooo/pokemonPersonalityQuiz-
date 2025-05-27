3920648ea02eae6fa75912cd7f328a1e68bae4fe
Yukitooo <yukibrent9@gmail.com>
2025-05-28 01:34:10 +0800

    I have put watermak that links to my github

--- src/components/Result.js
+++ src/components/Result.js
@@ -8,19 +8,28 @@ function Result({ resultType }) {
   if (!result) return <p>Loading result...</p>;
 
   return (
-    <div className="resultContent" style={{ border: `1rem solid ${result.color}` }}>
-      <div className="leftSide" style={{ borderRight: `2px ${result.color} solid` }}>
-        <img src={`${process.env.PUBLIC_URL}/${result.image}`} alt={result.name} />
-        <h1 style={{ color: result.color }} className="leftSideContent">{result.name}</h1>
-        <h3 style={{ color: result.color }} className="leftSideContent">{result.type}</h3>
-        <p className="leftSideContent tagline">{result.tagline}</p>
-      </div>
-      <div className="rightSide">
-        {result.description.map((desc, index) => (
-          <p key={index}>- {desc}</p>
-        ))}
-      </div>
+
+    <div className="resultContainer">
+        <div className="resultContent" style={{ border: `1rem solid ${result.color}` }}>
+        <div className="leftSide" style={{ borderRight: `2px ${result.color} solid` }}>
+            <img src={`${process.env.PUBLIC_URL}/${result.image}`} alt={result.name} />
+            <h1 style={{ color: result.color }} className="leftSideContent">{result.name}</h1>
+            <h3 style={{ color: result.color }} className="leftSideContent">{result.type}</h3>
+            <p className="leftSideContent tagline">{result.tagline}</p>
+        </div>
+        <div className="rightSide">
+            {result.description.map((desc, index) => (
+            <p key={index}>- {desc}</p>
+            ))}
+        </div>
+        </div>
+        <div className='watermark'>
+            <br></br>
+            <p>Developed by Yuki</p>
+            <p>Github Link: <a href="https://github.com/Yukitooo" target="_blank" rel="noopener noreferrer">Yukitooo</a></p>
+        </div>
     </div>
+    
   );
 }
 
